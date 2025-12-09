485b35ef37d027a069887aa4e520d7a1252d172e
Maksim Lin <maks@manichord.com>
2025-12-09 21:35:59 +1100

    placeholder for wide font (#31)

--- lib/pico_app.dart
+++ lib/pico_app.dart
@@ -3,15 +3,24 @@
 import 'package:flutter/material.dart';
 import 'package:picotracker_client/main_screen.dart';
 
-const AdvFontOffSet = 2;
+const AdvFontOffSet = 3;
 
 enum PtFont {
   Hourglass,
   YouSquared,
+  Wide,
   Ubuntu_Mono,
 }
 
 final fontNotifier = ValueNotifier<PtFont>(PtFont.Hourglass);
+
+extension PtFontX on PtFont {
+  String get fontFamily {
+    // Default to the declared font family name (spaces not underscores)
+    if (this == PtFont.Wide) return 'Hourglass';
+    return name.replaceAll("_", " ");
+  }
+}
 
 class PicoApp extends StatelessWidget {
   const PicoApp({super.key});
@@ -26,7 +35,7 @@ class PicoApp extends StatelessWidget {
           theme: ThemeData(
             colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
             useMaterial3: true,
-            fontFamily: fontNotifier.value.name.replaceAll("_", " "),
+            fontFamily: fontNotifier.value.fontFamily,
           ),
           home: const MainScreen(),
         );
